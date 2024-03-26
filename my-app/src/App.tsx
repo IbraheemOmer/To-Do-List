6a60d8ecfdc73a5f43f4c46976bcb6d28210f4c7
Ibraheem Omer <ibraheemomer.01@gmail.com>
2024-03-26 21:11:16 +0500

    To Do List Code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,168 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+const initialTodos: Todo[] = [
+  { id: "1", title: "Learn React", completed: true },
+  { id: "2", title: "Learn JS", completed: false },
+  { id: "3", title: "Learn HTML", completed: false },
+];
+
+export default function App() {
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+  function clearCompleted() {
+    setTodos((todos) => todos.filter((todo) => !todo.completed));
+  }
+
+  function handleToggleTodoCompleted(id: string) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  function handleAddTodo(todo: Todo) {
+    setTodos((todos) => [...todos, todo]);
+  }
+
+  function deleteTodo(id: string) {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1>Todo List</h1>
+      <AddForm onAddTodo={handleAddTodo} />
+      <TodoList
+        todos={todos}
+        onClick={deleteTodo}
+        onToggleTodo={handleToggleTodoCompleted}
+        onClear={clearCompleted}
+      />
     </div>
   );
 }
 
-export default App;
+function AddForm({ onAddTodo }: { onAddTodo: (todo: Todo) => void }) {
+  const [addtodo, setAddTodo] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!addtodo) {
+      return;
+    }
+
+    const id = crypto.randomUUID();
+
+    const newTodo: Todo = {
+      id,
+      title: addtodo,
+      completed: false,
+    };
+
+    onAddTodo(newTodo);
+
+    setAddTodo("");
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={addtodo}
+        onChange={(e) => setAddTodo(e.target.value)}
+        placeholder="Add Todo"
+      />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function TodoList({
+  todos,
+  onClick,
+  onToggleTodo,
+  onClear,
+}: {
+  todos: Todo[];
+  onClick: (id: string) => void;
+  onToggleTodo: (id: string) => void;
+  onClear: () => void;
+}) {
+  const [sort, setSort] = useState("All");
+  let sortedTodos: Todo[];
+  const completedTask = todos.filter((todo) => todo.completed).length;
+
+  if (sort === "Active") {
+    sortedTodos = todos.filter((todo) => !todo.completed);
+  } else if (sort === "Completed") {
+    sortedTodos = todos.filter((todo) => todo.completed);
+  } else {
+    sortedTodos = todos;
+  }
+
+  return (
+    <ul className="todo-list">
+      {sortedTodos.map((todo) => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          onClick={onClick}
+          onToggleTodo={onToggleTodo}
+        />
+      ))}
+      <div className="footer">
+        <p>{completedTask} Tasks Completed</p>
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Active">Active</option>
+          <option value="Completed">Completed</option>
+        </select>
+        <Button onClick={onClear}>Clear Completed</Button>
+      </div>
+    </ul>
+  );
+}
+
+function TodoItem({
+  todo,
+  onClick,
+  onToggleTodo,
+}: {
+  todo: Todo;
+  onClick: (id: string) => void;
+  onToggleTodo: (id: string) => void;
+}) {
+  return (
+    <li>
+      <div>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => onToggleTodo(todo.id)}
+        />
+        {todo.title}
+      </div>
+      <Button onClick={() => onClick(todo.id)}>🗑️</Button>
+    </li>
+  );
+}
+
+function Button({ children, onClick }: { children: React.ReactNode; onClick?: () => void; }) {
+  return (
+    <button type="button" className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
